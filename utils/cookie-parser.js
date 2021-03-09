4ec84789efe13c2ad525a8130553dc83d3608276
ravi-gajera27 <gajeraravi42@gmail.com>
2021-03-09 15:19:43 +0530

    commit

--- utils/cookie-parser.js
+++ utils/cookie-parser.js
@@ -3,11 +3,10 @@ const crypto = require('crypto-js')
 exports.extractCookie = async (req, res) =>
   new Promise(async (resolve) => {
     let cookie = req.cookies['firestep_access'];
-    console.log(cookie)
     try {
       let decrypt = await crypto.AES.decrypt(cookie, process.env.RES_SECRET);
       let decryptData = await JSON.parse(decrypt.toString(crypto.enc.Utf8));
-      console.log(decryptData)
+      console.log('desc',decryptData)
       resolve(decryptData);
     } catch (e) {
       console.log(e)
