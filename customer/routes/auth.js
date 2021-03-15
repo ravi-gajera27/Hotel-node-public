c7b03c7f57293ecb801d49aa5f1aee2eba31ede2
ravi-gajera27 <gajeraravi42@gmail.com>
2021-03-15 20:16:39 +0530

    commit

--- customer/routes/auth.js
+++ customer/routes/auth.js
@@ -5,6 +5,7 @@ const { protect } = require('../../middleware/customerAuth')
 router.post('/login', auth.login)
 router.post('/signup', auth.signup)
 router.get('/user', protect, auth.getUser)
+router.get('/verify-session', protect, auth.verifySession)
 router.put('/verify-otp', protect, auth.verifyOtp)
 
 module.exports = router
