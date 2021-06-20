bda55a0ae835b90877f80fbc8eae9da430bf26e8
ravi-gajera27 <gajeraravi42@gmail.com>
2021-06-20 14:54:53 +0530

    commit

--- admin/routes/auth.js
+++ admin/routes/auth.js
@@ -1,16 +1,22 @@
-const router = require('express').Router()
-const auth = require('../controllers/auth')
-const { protect } = require('../../middleware/adminAuth')
+const router = require("express").Router();
+const auth = require("../controllers/auth");
+const { protect } = require("../../middleware/adminAuth");
 
-router.post('/login', auth.login)
-router.post('/signup', auth.signup)
-router.get('/user', protect, auth.getUser)
-router.put('/verify-otp', protect, auth.verifyOtp)
-router.post('/rest-details', protect, auth.restaurantRegister)
-router.get('/admin', protect, auth.getAdminList)
-router.post('/admin', protect, auth.addAdmin)
-router.delete('/admin/:email', protect, auth.removeAdmin)
-router.put('/rest-details', protect, auth.updateRestaurantDetaials)
-router.post('/reset-password', protect, auth.resetPassword)
+router.post("/login", auth.login);
+router.post("/signup", auth.signup);
+router.get("/user", protect, auth.getUser);
+router.put("/verify-otp", protect, auth.verifyOtp);
+router.post("/rest-details", protect, auth.restaurantRegister);
+router.get("/admin", protect, auth.getAdminList);
+router.post("/admin", protect, auth.addAdmin);
+router.delete("/admin/:email", protect, auth.removeAdmin);
+router.put("/rest-details", protect, auth.updateRestaurantDetaials);
+router.post("/reset-password", protect, auth.resetPassword);
+router.put("/forgot-password", auth.forgotPasswordCheckMail);
+router.put(
+  "/forgot-password/verification-code",
+  auth.checkVerificationCodeForForgotPass
+);
+router.put("/change-password", auth.changePassword);
 
-module.exports = router
+module.exports = router;
