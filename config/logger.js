86b42bd5163aad208ca6d86e2e2537370307ffee
ravi-gajera27 <gajeraravi42@gmail.com>
2021-08-01 10:37:54 +0530

    commit

--- config/logger.js
+++ config/logger.js
@@ -5,31 +5,36 @@ require('winston-mongodb')
 let logger = null
 
 if (process.env.NODE_ENV == 'prod') {
-  logger = createLogger({
-    transports: [
-      new transports.MongoDB({
-        db: process.env.MONGODB_URL,
-        level: 'info',
-        format: combine(label, timestamp(), format.json()),
-        options: { useUnifiedTopology: true },
-      }),
-    ],
-  })
+  try {
+    logger = createLogger({
+      transports: [
+        new transports.MongoDB({
+          db: process.env.MONGODB_URL,
+          level: 'info',
+          format: combine(label, timestamp(), format.json()),
+          options: { useUnifiedTopology: true },
+        }),
+      ],
+    })
+  } catch (e) {
+    let ew = new Error(e)
+    console.log(ew.message)
+  }
 } else if (process.env.NODE_ENV == 'dev') {
   console.log('calll')
-  try{
-  logger = createLogger({
-    level: 'info',
-    format: combine(timestamp(), prettyPrint()),
-    transports: [
-      new transports.File({ filename: 'public/log' }),
-      new transports.Console(),
-    ],
-  })
-}catch(e){
-  let ew = new Error(e);
-  console.log(ew.message)
-}
+  try {
+    logger = createLogger({
+      level: 'info',
+      format: combine(timestamp(), prettyPrint()),
+      transports: [
+        new transports.File({ filename: 'public/log' }),
+        new transports.Console(),
+      ],
+    })
+  } catch (e) {
+    let ew = new Error(e)
+    console.log(ew.message)
+  }
 }
 
 module.exports = logger
