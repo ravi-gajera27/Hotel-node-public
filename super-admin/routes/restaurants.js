9231ea1ff93218f6919142d476dfd92bd2910b67
ravi-gajera27 <gajeraravi42@gmail.com>
2021-08-09 21:21:15 +0530

    commit

--- super-admin/routes/restaurants.js
+++ super-admin/routes/restaurants.js
@@ -5,6 +5,6 @@ const { protect } = require('../../middleware/superAdminAuth')
 router.get('/', protect, rest.getRestaurantsList);
 router.get('/request', protect, rest.getRestaurantsRequestList);
 router.put('/verify/:id', protect, rest.verifyRestaurantById);
-router.get('/:rest_id', protect, rest.verifyRestaurantById);
+router.get('/:rest_id', protect, rest.getRestaurantById);
 
 module.exports = router
